ce83ba0375b4e70add41563040d8d7834a46bc54
Kalkidan-Amare <kalkidanamare11a@gmail.com>
2025-01-02 11:43:51 +0300

    update the setup

--- API/main.go
+++ API/main.go
@@ -3,24 +3,27 @@ package main
 import (
 	"log"
 	"aastu_lib/router"
-	"aastu_lib/data"
 	"aastu_lib/config"
-
-	"github.com/gin-gonic/gin"
-
+	"aastu_lib/data" 
 )
 
-func main(){
-	// client, err := config.Init()
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	
-	// // data.SetUserCollection(client)
-	// // data.SetBookCollection(client)
-	r := gin.Default()
-	router.SetupBookRouter(r)
-	router.SetupUserRouter(r)
+func main() {
+	// Initialize the database
+	client, err := config.Init()
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
 
-	r.Run()	
+	// Set database collections
+	data.SetUserCollection(client)
+	data.SetBookCollection(client)
+	data.SetBorrowBooksCollection(client)
+	data.SetCheckinCollection(client)
+
+	// Setup router and run server
+	r := router.SetupRouter()
+	log.Println("Starting server on :8080...")
+	if err := r.Run(":8080"); err != nil {
+		log.Fatal("Server failed to start:", err)
+	}
 }
