0e485a0e1e8e24fdd9225f0ea0d8c95e4ad22c08
Kalkidan-Amare <kalkidanamare11a@gmail.com>
2025-01-21 12:23:44 +0300

    add loginadmin and checkstudent structure

--- API/models/user.go
+++ API/models/user.go
@@ -5,9 +5,20 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type User struct {
 	ID       primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
 	Username string             `bson:"username" json:"username"`
+	Email    string             `bson:"email" json:"email"`
 	StudentId string            `json:"student_id" bson:"student_id,omitemptly"`
     Password string             `bson:"password" json:"password"`
     Role     string             `bson:"role" json:"role"`
 	BorrowedBooks []primitive.ObjectID `bson:"borrowed_books" json:"borrowed_books"`
+	IscheckedIN bool 			`bson:"is_checked_in" json:"is_checked_in"`
 
+}
+
+type LoginAdmin struct {
+	Email	 string `json:"email"`
+	Password string `json:"password"`
+}
+
+type CheckStudent struct {
+	StudentId string `json:"student_id"`
 }
